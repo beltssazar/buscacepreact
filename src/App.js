f83ed3bf28c1b363e73201dcddea76c4e299210f
Daniel Fernando <danielfernando@camp.local>
2022-06-17 09:33:26 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import { FiSearch } from 'react-icons/fi';
+import "./style.css";
 
+import api from './services/api'; 
 function App() {
+
+  const [input, setInput] = useState('')
+
+  async function handleBotao(){
+    //06704215/json/
+
+    if(input === ""){
+      alert("Preencha o campo com algum CEP!")
+      return;
+    }
+
+    try {
+      const response = await api.get('${input}/json')
+      console.log(response.data)
+
+    } catch{
+      alert("Erro ao buscar o CEP!");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input type="text"
+        placeholder="Informe o seu cep"
+        value={input}
+        onChange={(event) => setInput(event.target.value)}>
+        </input>
+        <button className="botaoBusca" onClick={handleBotao}>
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+      </div>
+        <main className='main'>
+          <h2>CEP: 06704215</h2>
+
+          <span>Rua: Algum Teste</span>
+          <span>Complemento: Algum Complemanto</span>
+          <span>Bairro: Parque Miguel Mirizola</span>
+          <span>Cotia - SP</span>
+        </main>
     </div>
+
+   
   );
 }
 
